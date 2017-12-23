bff635cf65860bc943efc1baed010694c1b3248c
Matt <matt@matthewjorr.com>
2017-12-23 16:41:41 -0500

    added concurrency+wg

--- create_db.go
+++ create_db.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"strconv"
 	"time"
+	"sync"
 )
 
 type NPI_Taxonomy struct {
@@ -18,6 +19,12 @@ type NPI_Taxonomy struct {
 }
 
 func main() {
+	start := -1
+	loop := &start
+
+	var wg sync.WaitGroup
+
+	fmt.Println("Connecting to database...")
 	st := time.Now()
 	db, err := setupDB()
 	if err != nil {
@@ -25,18 +32,17 @@ func main() {
 	}
 	defer db.Close()
 
+	fmt.Println("Opening file...")
 	file, err := os.Open("all_taxonomies.csv")
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
 	defer file.Close()
-	data := csv.NewReader(file)
-	fmt.Println("Loading Data...")
 
-	loop := -1
+	fmt.Println("Reading file...")
+	data := csv.NewReader(file)
 	for {
-		loop += 1
 		record, err := data.Read()
 		if err == io.EOF {
 			break
@@ -45,12 +51,17 @@ func main() {
 			log.Fatal(err)
 		}
 		if i, err := strconv.Atoi(record[0]); err == nil {
-			err = addNPI(db, i, record[1], loop)
-			if err != nil {
-				log.Fatal(err)
-		}
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				err = addNPI(db, i, record[1], loop)
+				if err != nil {
+					log.Fatal(err)
+				}
+			}()
 		}
 	}
+	wg.Wait()
 	fmt.Printf("Success!\nStart time: %v\nEnd time: %v\n", st.Local(), time.Now().Local())
 }
 
@@ -74,13 +85,14 @@ func setupDB() (*bolt.DB, error) {
 	return db, nil
 }
 
-func addNPI(db *bolt.DB, npi int, taxonomy string, loop int) error {
+func addNPI(db *bolt.DB, npi int, taxonomy string, loop *int) error {
+	*loop += 1
 	entry := NPI_Taxonomy{NPI: npi, Taxonomy: taxonomy}
 	encoded, err := json.Marshal(entry)
 	if err != nil {
 		return fmt.Errorf("could not marshal entry json: %v", err)
 	}
-	err = db.Update(func(tx *bolt.Tx) error {
+	err = db.Batch(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte("DB")).Bucket([]byte("NPI"))
 		id, _ := b.NextSequence()
 		b.Put([]byte(strconv.FormatInt(int64(id), 32)), encoded)
@@ -88,8 +100,7 @@ func addNPI(db *bolt.DB, npi int, taxonomy string, loop int) error {
 			return fmt.Errorf("could not insert entry: %v", err)
 		}
 		return nil
-		
 	})
-	fmt.Printf("Added NPI Entry %v\n", loop)
+	fmt.Printf("Added NPI Entry %v\n", *loop)
 	return err
 }
